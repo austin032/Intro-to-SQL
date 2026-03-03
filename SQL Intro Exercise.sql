de2d646f8134cce1474576c0ec45c5c7b715aba0
Austin Okyere <okyereaustin@gmail.com>
2026-03-03 16:17:21 -0700

    Fixed sql intro exercise

--- SQL Intro Exercise.sql
+++ SQL Intro Exercise.sql
@@ -7,13 +7,13 @@
 -- find all products that cost $1400
  SELECT * FROM bestbuy.products WHERE products.Price = 1400;
 -- find all products that cost $11.99 or $13.99
- SELECT * FROM bestbuy.products WHERE products.Price = 11.99 OR cost = 13.99;
+ SELECT * FROM bestbuy.products WHERE products.Price = 11.99 OR products.Price = 13.99;
 -- find all products that do NOT cost 11.99 - using NOT
  SELECT * FROM bestbuy.products WHERE NOT products.Price = 11.99; 
 -- find  all products and sort them by price from greatest to least
  SELECT * FROM bestbuy.products ORDER BY products.Price DESC; 
 -- find all employees who don't have a middle initial
- SELECT * FROM bestbuy.employees WHERE employees.MiddleInitial = '';
+ SELECT * FROM bestbuy.employees WHERE employees.MiddleInitial IS NULL;
 -- find distinct product prices
  SELECT DISTINCT bestbuy.products.Price FROM bestbuy.products;
 -- find all employees whose first name starts with the letter ‘j’
@@ -25,6 +25,6 @@
 -- find the average price of all products 
  SELECT AVG(products.Price) FROM bestbuy.products;
 -- find all Geek Squad employees who don't have a middle initial 
- SELECT * FROM bestbuy.employees WHERE employees.MiddleInitial = '' AND employees.Title = 'Geek Squad';
+ SELECT * FROM bestbuy.employees WHERE employees.MiddleInitial IS NULL AND employees.Title = 'Geek Squad';
 -- find all products from the products table whose stock level is in the range  -- of 500 to 1200. Order by Price from least to greatest. **Use the between keyword** 
 SELECT * FROM bestbuy.products WHERE products.StockLevel BETWEEN 500 AND 1200 ORDER BY price;
